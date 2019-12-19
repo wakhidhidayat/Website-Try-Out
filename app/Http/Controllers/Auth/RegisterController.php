cb2b81f4a70c15e2501629d48d4bbafb8c45533b
dstwn <me@dstwn.id>
2019-12-19 14:07:44 +0700

    updated fix some bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,7 +79,7 @@ class RegisterController extends Controller
             "kelompok" => "required",
             "email" => "required|max:191|unique:users",
             "password" => "required|min:8|max:191",
-            "password_confirmation" => "required|same:password"
+            "password_confirmation" => "required|same:password",
         ])->validate();
 
         $maxpeserta = User::max('no_ujian');
@@ -89,7 +89,6 @@ class RegisterController extends Controller
         $noujian = sprintf("%09s",abs($maxpeserta +1));
 
         $request = request();
-        //$file = Image::make($request->file('foto'))->resize(240,320)->insert('pas_foto','public');
         $file = $request->file('foto')->store('pas_foto', 'public');
         return User::create([
             'nama' => $data['nama'],
